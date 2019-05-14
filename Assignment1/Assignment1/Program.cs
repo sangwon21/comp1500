5383ac19d0946ca34651c148c4a96e3b2a4aef1f
junothyme <junothyme@gmail.com>
2019-05-14 21:41:23 +0900

    Uncommenting Comments - Assignment 1

--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -8,15 +8,14 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
-            // using 문에 대해서는 아직 고민하지 마세요. 나중에 배웁니다.
             using (StreamReader READER = new StreamReader(Console.OpenStandardInput()))
             using (StreamWriter WRITER = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true })
             {
                 Console.WriteLine("Enter your 5 digits for PrintIntegers():");
                 Assignment1.PrintIntegers(READER, WRITER);
 
-                //Console.WriteLine("Enter your 5 digits for PrintStats():");
-                //Assignment1.PrintStats(READER, WRITER);
+                Console.WriteLine("Enter your 5 digits for PrintStats():");
+                Assignment1.PrintStats(READER, WRITER);
             }
         }
     }
