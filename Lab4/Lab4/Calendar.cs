708adfcd014ecfe0d9e6a6fee8f79fc578c10623
junothyme <junothyme@gmail.com>
2019-05-27 10:49:24 +0900

    Adding IsLeapYear() and GetDaysInMonth() - Lab 4

---
+++ Lab4/Lab4/Calendar.cs
@@ -0,0 +1,46 @@
+﻿namespace Lab4
+{
+    public static class Calendar
+    {
+        public static bool IsLeapYear(uint year)
+        {
+            if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static int GetDaysInMonth(uint year, uint month)
+        {
+            if(IsLeapYear(year) && month == 2)
+            {
+                return 29;
+            }
+            else if(IsLeapYear(year) == false && month == 2)
+            {
+                return 28;
+            }
+
+            switch(month)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    return 31;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    return 30;
+                default:
+                    return -1;
+            }
+        }
+    }
+}
