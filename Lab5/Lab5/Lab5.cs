fb2365962d628191c0212582c1dceff774088fb7
junothyme <junothyme@gmail.com>
2019-06-03 16:37:52 +0900

    Setting the Project and Implementing TryFixData() - Lab 5

---
+++ Lab5/Lab5/Lab5.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Lab5
+{
+    public static class Lab5
+    {
+        public static bool TryFixData(uint[] usersPerDay, double[] revenuePerDay)
+        {
+            if (usersPerDay.Length != revenuePerDay.Length)
+            {
+                return false;
+            }
+
+            bool bJudge = false;
+
+            for (uint i = 0; i < usersPerDay.Length; i++)
+            {
+                double tmp = 0.0;
+
+                if (usersPerDay[i] > 1000)
+                {
+                    tmp = 245743 + usersPerDay[i] / 4.0;
+                    tmp = tmp * 100;
+                    tmp += 0.5;
+                    long temp = (long)tmp;
+                    tmp = (double)temp / 100.0;
+                }
+                else if (usersPerDay[i] > 100)
+                {
+                    tmp = usersPerDay[i] * usersPerDay[i] / 4.0 - 2.0 * usersPerDay[i] - 2007;
+                    tmp = tmp * 100;
+                    tmp += 0.5;
+                    long temp = (long)tmp;
+                    tmp = (double)temp / 100.0;
+                }
+                else if (usersPerDay[i] > 10)
+                {
+                    tmp = 16 * usersPerDay[i] / 5.0 - 27;
+                    tmp = tmp * 100;
+                    tmp += 0.5;
+                    long temp = (long)tmp;
+                    tmp = (double)temp / 100.0;
+                }
+                else
+                {
+                    tmp = usersPerDay[i] / 2.0;
+                    tmp = tmp * 100;
+                    tmp += 0.5;
+                    long temp = (long)tmp;
+                    tmp = (double)temp / 100.0;
+                }
+
+                if (tmp != revenuePerDay[i])
+                {
+                    revenuePerDay[i] = tmp;
+                    bJudge = true;
+                }
+            }
+
+            return bJudge;
+        }
+
+        public static int GetInvalidEntryCount(uint[] usersPerDay, double[] revenuePerDay)
+        {
+            return -1;
+        }
+
+        public static double CalculateTotalRevenue(double[] revenuePerDay, uint start, uint end)
+        {
+            return 0.0;
+        }
+    }
+}
--- Lab5/Lab5/Program.cs
+++ Lab5/Lab5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Lab5
 {
@@ -6,7 +7,20 @@ namespace Lab5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int NUMBER_OF_DAYS = 15;
+
+            uint[] usersPerDay = new uint[NUMBER_OF_DAYS] { 0, 3, 5, 10, 34, 66, 89, 100, 101, 321, 657, 1000, 1032, 4520, 6578 };
+            double[] revenuePerDay = new double[NUMBER_OF_DAYS] { 0, 1.50, 2.50, 5.00, 81.80, 184.20, 257.80, 293.00, 341.25, 23111.25, 104591.25, 245993.00, 246001.00, 246873.00, 247387.50 };
+
+            bool bFixed = Lab5.TryFixData(usersPerDay, revenuePerDay);
+            Debug.Assert(!bFixed);
+
+            revenuePerDay[0] = 10000.2;
+            revenuePerDay[6] = 156;
+            revenuePerDay[11] = 45628.34;
+
+            bFixed = Lab5.TryFixData(usersPerDay, revenuePerDay);
+            Debug.Assert(bFixed);
         }
     }
 }
