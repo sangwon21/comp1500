56adf3530af21e40ef96b1a74ab81dc4b6f4432d
junothyme <junothyme@gmail.com>
2019-06-10 10:07:13 +0900

    Implementing Rotate90Degrees() - Lab 6

--- Lab6/Lab6/Lab6.cs
+++ Lab6/Lab6/Lab6.cs
@@ -2,6 +2,22 @@
 {
     public static class Lab6
     {
+        public static int[,] Rotate90Degrees(int[,] vector)
+        {
+            int row = vector.GetLength(0);
+            int column = vector.GetLength(1);
 
+            int[,] newVector = new int[column, row];
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    newVector[j, (row - 1) - i] = vector[i, j];
+                }
+            }
+
+            return newVector;
+        }
     }
 }
--- Lab6/Lab6/Program.cs
+++ Lab6/Lab6/Program.cs
@@ -6,7 +6,40 @@ namespace Lab6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] data = getTestArray();
+
+            int[,] rotated = Lab6.Rotate90Degrees(data);
+            printArray(rotated);
+
+        }
+
+        private static int[,] getTestArray()
+        {
+            return new int[5, 6]
+            {
+                { 1, 2, 3, 4, 5, 6 },
+                { 11, 12, 13, 14, 15, 16 },
+                { 21, 22, 23, 24, 25, 26 },
+                { 31, 32, 33, 34, 35, 36 },
+                { 41, 42, 43, 44, 45, 46 }
+            };
+        }
+
+        private static void printArray(int[,] data)
+        {
+            Console.WriteLine("---------------------------------");
+
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                for (int j = 0; j < data.GetLength(1); j++)
+                {
+                    Console.Write("{0, -4} ", data[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("---------------------------------");
         }
     }
 }
