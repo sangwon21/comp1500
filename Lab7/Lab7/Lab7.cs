ff1cd16d509d9907019ed81c5c2fd2ae5c4b25d8
junothyme <junothyme@gmail.com>
2019-07-01 13:33:19 +0900

    Applying Coding Standard - Lab 7

--- Lab7/Lab7/Lab7.cs
+++ Lab7/Lab7/Lab7.cs
@@ -4,7 +4,7 @@
     {
         public static bool PlayGame(uint[] array)
         {
-            if(array.Length == 0 || array.Length == 1)
+            if (array.Length == 0 || array.Length == 1)
             {
                 return false;
             }
@@ -13,22 +13,22 @@
 
         public static bool Helper(uint[] array, int currentIndex, int previousIndex, int plusMinus)
         {
-            if(currentIndex <= 0 || currentIndex >= array.Length)
+            if (currentIndex <= 0 || currentIndex >= array.Length)
             {
                 return false;
             }
 
-            if(array[currentIndex] == 0)
+            if (array[currentIndex] == 0)
             {
                 return true;
             }
 
-            if(plusMinus == 0 && array[currentIndex] == array[previousIndex])
+            if (plusMinus == 0 && array[currentIndex] == array[previousIndex])
             {
                 return Helper(array, currentIndex + (int)array[currentIndex], currentIndex, 0);
             }
 
-            if(plusMinus == 1 && array[currentIndex] == array[previousIndex])
+            if (plusMinus == 1 && array[currentIndex] == array[previousIndex])
             {
                 return Helper(array, currentIndex - (int)array[currentIndex], currentIndex, 1);
             }
