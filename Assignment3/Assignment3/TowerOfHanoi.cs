1d3a087376fe72fd10985efe3ca8cf9cce59e11d
junothyme <junothyme@gmail.com>
2019-07-02 22:24:02 +0900

    Implementing the Basic Setting - Assignment 3

---
+++ Assignment3/Assignment3/TowerOfHanoi.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Assignment3
+{
+    public static class TowerOfHanoi
+    {
+
+        public static int GetNumberOfSteps(int numDiscs)
+        {
+            return -1;
+        }
+
+        public static List<List<int>[]> SolveTowerOfHanoi(int numDiscs)
+        {
+            return null;
+        }
+    }
+}
