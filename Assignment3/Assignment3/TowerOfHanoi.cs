dbc395a4786931f0fdbf7d09e723f00700192e1d
junothyme <junothyme@gmail.com>
2019-07-03 18:23:07 +0900

    Introducing Recursive Function - Assignment 3

--- Assignment3/Assignment3/TowerOfHanoi.cs
+++ Assignment3/Assignment3/TowerOfHanoi.cs
@@ -7,12 +7,75 @@ namespace Assignment3
 
         public static int GetNumberOfSteps(int numDiscs)
         {
-            return -1;
+            if (numDiscs < 0)
+            {
+                return -1;
+            }
+
+            int steps = 1;
+            for (int i = 0; i < numDiscs; i++)
+            {
+                steps *= 2;
+            }
+
+            return steps - 1;
         }
 
         public static List<List<int>[]> SolveTowerOfHanoi(int numDiscs)
         {
-            return null;
+            List<List<int>[]> list = new List<List<int>[]>();
+            List<int> first = new List<int>(numDiscs);
+            List<int> second = new List<int>(numDiscs);
+            List<int> third = new List<int>(numDiscs);
+
+            for(int i = numDiscs; i > 0; i--)
+            {
+                first.Add(i);
+            }
+
+            List<int>[] array = new List<int>[3];
+
+            array[0] = first;
+            array[1] = second;
+            array[2] = third;
+
+            list.Add(array);
+
+            hanoi( list, numDiscs, 0, 1, 2);
+
+            return list;
         }
+
+        public static void hanoi( List<List<int>[]> list, int n, int from, int by,int to)
+        {
+            if (n == 0)
+            {
+                return;
+            }
+            hanoi( list, n - 1, from, to, by);
+
+            List<int>[] newList = new List<int>[3];
+            List<int> first = new List<int>(list[list.Count - 1][0]);
+            List<int> second = new List<int>(list[list.Count - 1][1]);
+            List<int> third = new List<int>(list[list.Count - 1][2]);
+
+            newList[0] = first;
+            newList[1] = second;
+            newList[2] = third;
+
+            List<int> fromList = newList[from];
+            int number = fromList[fromList.Count - 1];
+            fromList.Remove(number);
+
+            List<int> toList = newList[to];
+            toList.Add(number);
+
+            list.Add(newList);
+
+            hanoi( list, n - 1, by, from, to);
+
+        }
+
+
     }
 }
