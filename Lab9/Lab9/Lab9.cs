09ed7f00cfd2cc9de4e91e466a257b4b66006080
junothyme <junothyme@gmail.com>
2019-07-15 13:52:00 +0900

    Implementing MergeLists - Lab 9

---
+++ Lab9/Lab9/Lab9.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+namespace Lab9
+{
+    public static class Lab9
+    {
+        public static List<int> MergeLists(List<int> sortedList1, List<int> sortedList2)
+        {
+            List<int> combinedList = new List<int>(sortedList1.Count + sortedList2.Count);
+
+            int i = 0;
+            int j = 0;
+
+            while (i < sortedList1.Count && j < sortedList2.Count)
+            {
+                if (sortedList1[i] < sortedList2[j])
+                {
+                    combinedList.Add(sortedList1[i]);
+                    i++;
+                }
+                else
+                {
+                    combinedList.Add(sortedList2[j]);
+                    j++;
+                }
+            }
+
+            if (i < sortedList1.Count)
+            {
+                while (i < sortedList1.Count)
+
+                {
+                    combinedList.Add(sortedList1[i]);
+                    i++;
+                }
+            }
+            if (j < sortedList2.Count)
+            {
+                while (j < sortedList2.Count)
+
+                {
+                    combinedList.Add(sortedList2[j]);
+                    j++;
+                }
+            }
+
+            return combinedList;
+        }
+
+        public static Dictionary<string, int> CombineListsToDictionary(List<string> keys, List<int> values)
+        {
+            return null;
+        }
+
+        public static Dictionary<string, decimal> MergeDictionaries(Dictionary<string, int> numerators, Dictionary<string, int> denominators)
+        {
+            return null;
+        }
+    }
+}
--- Lab9/Lab9/Program.cs
+++ Lab9/Lab9/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
 
 namespace Lab9
 {
@@ -6,7 +10,20 @@ namespace Lab9
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> list1 = new List<int> { 1, 1, 4, 7, 8, 11, 20 };
+            List<int> list2 = new List<int> { 2, 3, 4, 5, 10, 15, 16, 21, 22 };
+
+            List<int> combinedList = Lab9.MergeLists(list1, list2);
+            List<int> expectedList = new List<int> { 1, 1, 2, 3, 4, 4, 5, 7, 8, 10, 11, 15, 16, 20, 21, 22 };
+
+            Console.WriteLine($"[ {string.Join(", ", combinedList)} ]");
+
+            for (int i = 0; i < expectedList.Count; i++)
+            {
+                Debug.Assert(expectedList[i] == combinedList[i]);
+            }
+
+            Console.WriteLine("End of the Test");
         }
     }
 }
