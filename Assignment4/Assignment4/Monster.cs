385b644007f9f8df2ae01ad575b800e4bed6a6b8
junothyme <junothyme@gmail.com>
2019-07-22 22:20:11 +0900

    Implementing EElementType and Monster - Assignment 4

---
+++ Assignment4/Assignment4/Monster.cs
@@ -0,0 +1,70 @@
+﻿namespace Assignment4
+{
+    public class Monster
+    {
+        public Monster(string name, EElementType elementType, int health, int attack, int defense)
+        {
+            Name = name;
+            ElementType = elementType;
+            Health = health;
+            AttackStat = attack;
+            DefenseStat = defense;
+        }
+
+        public string Name
+        {
+            get; private set;
+        }
+
+        public EElementType ElementType
+        {
+            get; private set;
+        }
+
+        public int Health
+        {
+            get; private set;
+        }
+
+        public int AttackStat
+        {
+            get; private set;
+        }
+
+        public int DefenseStat
+        {
+            get; private set;
+        }
+
+        public void TakeDamage(int amount)
+        {
+            Health -= amount;
+        }
+
+        public void Attack(Monster otherMonster)
+        {
+            int basicAttack = AttackStat - otherMonster.DefenseStat;
+
+            if(otherMonster.ElementType == ElementType || ((ElementType == EElementType.Water ||ElementType == EElementType.Ground) 
+                && (otherMonster.ElementType == EElementType.Water || otherMonster.ElementType == EElementType.Ground)))
+            {
+                otherMonster.Health -= basicAttack;
+                return;
+            }
+            else if((ElementType == EElementType.Fire && otherMonster.ElementType == EElementType.Wind) || (ElementType == EElementType.Water && otherMonster.ElementType == EElementType.Fire)
+                || (ElementType == EElementType.Ground && otherMonster.ElementType == EElementType.Fire) || 
+                (ElementType == EElementType.Wind && (otherMonster.ElementType == EElementType.Water || otherMonster.ElementType == EElementType.Ground)))
+            {
+                double changedAttack = basicAttack * 1.5;
+                otherMonster.Health -= (int)changedAttack;
+                return;
+            }
+            else
+            {
+                double changedAttack = basicAttack * 0.5;
+                otherMonster.Health -= (int)changedAttack;
+                return;
+            }
+        }
+    }
+}
--- Assignment4/Assignment4/Program.cs
+++ Assignment4/Assignment4/Program.cs
@@ -6,7 +6,16 @@ namespace Assignment4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Monster monster1 = new Monster("Monster1", EElementType.Fire, 80, 5, 0);
+            Monster monster2 = new Monster("Monster2", EElementType.Fire, 80, 5, 0);
+            Monster monster3 = new Monster("Monster3", EElementType.Wind, 80, 7, 1);
+
+            monster1.Attack(monster2); // monster2.Health가 75이 됨
+            Console.WriteLine(monster2.Health);
+            monster1.Attack(monster3); // monster3.Health가 74가 됨
+            Console.WriteLine(monster3.Health);
+            monster3.Attack(monster1); // monster1.Health가 77이 됨
+            Console.WriteLine(monster1.Health);
         }
     }
 }
