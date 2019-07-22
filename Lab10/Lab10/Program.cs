e757efe442730d03059a4e2a97963186ed79a9e2
junothyme <junothyme@gmail.com>
2019-07-22 11:43:50 +0900

    Implementing RightTriangle Class - Lab 10

--- Lab10/Lab10/Program.cs
+++ Lab10/Lab10/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Lab10
 {
@@ -8,7 +9,37 @@ namespace Lab10
         {
             Rectangle rectangle1 = new Rectangle(5, 5);
 
-            Console.WriteLine(rectangle1.Width);
+            Debug.Assert(rectangle1.Width == 5);
+            Debug.Assert(rectangle1.Height == 5);
+
+            Debug.Assert(rectangle1.GetPerimeter() == 20.000);
+            Debug.Assert(rectangle1.GetArea() == 25.000);
+
+            Rectangle rectangle2 = new Rectangle(10, 4);
+
+            Debug.Assert(rectangle2.Width == 10);
+            Debug.Assert(rectangle2.Height == 4);
+
+            Debug.Assert(rectangle2.GetPerimeter() == 28.000);
+            Debug.Assert(rectangle2.GetArea() == 40.000);
+
+            RightTriangle triangle1 = new RightTriangle(4, 7);
+
+            Debug.Assert(triangle1.Width == 4);
+            Debug.Assert(triangle1.Height == 7);
+
+            Debug.Assert(triangle1.GetPerimeter() == 19.062);
+            Debug.Assert(triangle1.GetArea() == 14.000);
+
+            RightTriangle triangle2 = new RightTriangle(7, 2);
+
+            Debug.Assert(triangle2.Width == 7);
+            Debug.Assert(triangle2.Height == 2);
+
+            Debug.Assert(triangle2.GetPerimeter() == 16.280);
+            Debug.Assert(triangle2.GetArea() == 7.000);
+
+            Console.WriteLine("The End");
         }
     }
 }
---
+++ Lab10/Lab10/RightTriangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+public class RightTriangle
+{
+    public RightTriangle(uint width, uint height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public uint Width
+    {
+        get; private set;
+    }
+    public uint Height
+    {
+        get; private set;
+    }
+
+    public double GetPerimeter()
+    {
+        double thirdSide = Math.Sqrt(Height * Height + Width * Width);
+        double perimeter = Width + Height + thirdSide;
+        perimeter = (uint)(perimeter * 1000 + 0.5) / 1000.0;
+
+        return perimeter;
+    }
+
+    public double GetArea()
+    {
+        double area = (Width * (double)Height) / 2;
+        area = (uint)(area * 1000 + 0.5) / 1000.0;
+
+        return area;
+    }
+}
