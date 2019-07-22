950f2f6f08b689b6bdf4a099354cc50657bb4751
junothyme <junothyme@gmail.com>
2019-07-22 22:47:25 +0900

    Implementing LoadMonsters() in Arena and Adding monsters.txt - Assignment 4

---
+++ Assignment4/Assignment4/Arena.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System;
+
+namespace Assignment4
+{
+    class Arena
+    {
+        public Arena(string arenaName, uint capacity)
+        {
+            ArenaName = arenaName;
+            Capacity = capacity;
+        }
+
+        public uint Capacity
+        {
+            get; private set;
+        }
+
+        public string ArenaName
+        {
+            get; private set;
+        }
+
+        public uint Turns
+        {
+            get; private set;
+        }
+
+        public uint MonsterCount
+        {
+            get; private set;
+        }
+
+        private List<Monster> mMonsters;
+
+        public void LoadMonsters(string filePath)
+        {
+            string[] monsters = File.ReadAllLines(filePath);
+            foreach(var monster in monsters)
+            {
+                string[] attribute = monster.Split(',');
+                string name = attribute[0];
+                EElementType elementType;
+                int health;
+                int attackStat;
+                int defenseStat;
+                Enum.TryParse(attribute[1], out elementType);
+                int.TryParse(attribute[2], out health);
+                int.TryParse(attribute[3], out attackStat);
+                int.TryParse(attribute[4], out defenseStat);
+
+                Monster created = new Monster(name, elementType, health, attackStat, defenseStat);
+                if(mMonsters.Count <= Capacity)
+                {
+                    mMonsters.Add(created);
+                }
+            }
+        }
+    }
+}
--- Assignment4/Assignment4/Program.cs
+++ Assignment4/Assignment4/Program.cs
@@ -6,16 +6,20 @@ namespace Assignment4
     {
         static void Main(string[] args)
         {
-            Monster monster1 = new Monster("Monster1", EElementType.Fire, 80, 5, 0);
-            Monster monster2 = new Monster("Monster2", EElementType.Fire, 80, 5, 0);
-            Monster monster3 = new Monster("Monster3", EElementType.Wind, 80, 7, 1);
+            //Monster monster1 = new Monster("Monster1", EElementType.Fire, 80, 5, 0);
+            //Monster monster2 = new Monster("Monster2", EElementType.Fire, 80, 5, 0);
+            //Monster monster3 = new Monster("Monster3", EElementType.Wind, 80, 7, 1);
 
-            monster1.Attack(monster2); // monster2.Health가 75이 됨
-            Console.WriteLine(monster2.Health);
-            monster1.Attack(monster3); // monster3.Health가 74가 됨
-            Console.WriteLine(monster3.Health);
-            monster3.Attack(monster1); // monster1.Health가 77이 됨
-            Console.WriteLine(monster1.Health);
+            //monster1.Attack(monster2); // monster2.Health가 75이 됨
+            //Console.WriteLine(monster2.Health);
+            //monster1.Attack(monster3); // monster3.Health가 74가 됨
+            //Console.WriteLine(monster3.Health);
+            //monster3.Attack(monster1); // monster1.Health가 77이 됨
+            //Console.WriteLine(monster1.Health);
+
+            Arena arena = new Arena("Scorching Rocks", 8);
+
+            arena.LoadMonsters("monsters.txt");
         }
     }
 }
