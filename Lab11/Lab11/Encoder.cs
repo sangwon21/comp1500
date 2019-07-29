4a0966917eb9989bfcd93a924e6c68502ebb0308
junothyme <junothyme@gmail.com>
2019-07-29 10:51:06 +0900

    Implementing TryEncode() - Lab 11

---
+++ Lab11/Lab11/Encoder.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+
+namespace Lab11
+{
+    public static class Encoder
+    {
+        public static bool TryEncode(Stream input, Stream output)
+        {
+            if(input.Length == 0)
+            {
+                return false;
+            }
+
+            int previous = -1;
+            uint count = 0;
+            for(int i= 0; i < input.Length; i++)
+            {
+                int value = input.ReadByte();
+                if(previous == -1)
+                {
+                    previous = value;
+                    count++;
+                    continue;
+                }
+                if(value != previous)
+                {
+                    output.WriteByte((byte)previous);
+                    output.WriteByte((byte)count);
+                    previous = value;
+                    count = 0;
+                    continue;
+                }
+                count++;
+            }
+            return false;
+        }
+
+        public static bool TryDecode(Stream input, Stream output)
+        {
+            return false;
+        }
+    }
+}
